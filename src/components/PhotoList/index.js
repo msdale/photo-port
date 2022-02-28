825e9c340e537a1004e19655507b613f15bad3dc
Mark Dale <msdaledad@gmail.com>
2022-02-27 20:53:42 -0500

    included ContactForm

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -55,10 +55,23 @@ const PhotoList = ({ category }) => {
       category: 'portraits',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie'
-    },
-    {
-      name: 'Pancakes',
-      category: 'food',
+    },import React from 'react'
+    import { render, cleanup } from '@testing-library/react'
+    import '@testing-library/jest-dom/extend-expect'
+    import PhotoList from '../'
+    
+    afterEach(cleanup)
+    
+    describe('PhotoList is rendering', () => {
+      it('renders', () => {
+        render(<PhotoList />);
+      });
+    
+      it('renders', () => {
+        const { asFragment } = render(<PhotoList />)
+        expect(asFragment()).toMatchSnapshot()
+      });
+    });
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie'
     },
