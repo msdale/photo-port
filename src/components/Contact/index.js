cb8642ec12fa4ad1f30bb087738417973b7ad9dd
Mark Dale <msdaledad@gmail.com>
2022-02-28 11:31:57 -0500

    completed 20.4.4

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 function ContactForm() {
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  const { name, email, message } = formState;
+  
+  // sync the input form of the psuedo DOM with the "state" of the browser DOM 
+  function handleChange(e) {
+    setFormState({...formState, [e.target.name]: e.target.value })
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  };
+
   // JSX
   return (
     <section>
       <h1>Contact me</h1>
-      <form id="contact-form">
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
+          <input type="text" defaultValue={name} name="name" onChange={handleChange} />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" />
+          <input type="email" defaultValue={email} name="email" onChange={handleChange} />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
+          <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
         </div>
         <button type="submit">Submit</button>
       </form>
