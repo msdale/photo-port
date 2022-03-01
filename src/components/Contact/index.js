5c1e7823c5edb0b4497fee916a32cd287226d9c1
Mark Dale <msdaledad@gmail.com>
2022-03-01 17:20:53 -0500

    added conditional rendering

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,33 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
+
 function ContactForm() {
+  const [errorMessage, setErrorMessage] = useState('');
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
   const { name, email, message } = formState;
-  
+
   // sync the input form of the psuedo DOM with the "state" of the browser DOM 
   function handleChange(e) {
-    setFormState({...formState, [e.target.name]: e.target.value })
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      console.log("Valid Email? " + isValid);
+      // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+    console.log('errorMessage', errorMessage);
   };
 
   function handleSubmit(e) {
@@ -28,7 +50,12 @@ function ContactForm() {
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
+          <textarea defaultValue={message} name="message" onChange={handleChange} rows="5" />
+          {errorMessage && (
+            <div>
+              <p className="error-text">{errorMessage}</p>
+            </div>
+          )}
         </div>
         <button type="submit">Submit</button>
       </form>
