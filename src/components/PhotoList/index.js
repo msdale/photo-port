5b422ca6efead13204447442be060fe0ed590914
Mark Dale <msdaledad@gmail.com>
2022-03-03 12:12:18 -0500

    cleanup...intermediate 20.5.4

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
+import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
+  const [currentPhoto, setCurrentPhoto] = useState();
+  const toggleModal = (image, i) => {
+    setCurrentPhoto({ ...image, index: i });
+  }
+
   const [photos] = useState([
     {
       name: 'Grocery aisle',
@@ -122,12 +128,14 @@ const PhotoList = ({ category }) => {
 
   return (
     <div>
+      <Modal currentPhoto={currentPhoto} />
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
-            src={require(`../../assets/small/${category}/${i}.jpg`).default}
+            src={require(`../../assets/small/${category}/${i}.jpg`)}
             alt={image.name}
             className="img-thumbnail mx-1"
+            onClick={() => toggleModal(image, i)}
             key={image.name}
           />
         ))}
